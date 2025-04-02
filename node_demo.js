b828b952625895603e580e1fe501bff20b78501a
mca24082 <mca24082@siescoms.in>
2025-04-02 12:52:49 +0530

    Changes done

---
+++ node_demo.js
@@ -0,0 +1,29 @@
+// Printing a simple message
+console.log("Hello, World!");
+
+// Printing numbers
+console.log(42);
+
+// Printing multiple values
+console.log("The answer is:", 42);
+
+// Using template literals
+let name = "Anuja";
+console.log(`Hello, ${name}! Welcome to JavaScript.`);
+
+// Printing an object
+let user = { name: "Anuja", age: 25 };
+console.log("User Info:", user);
+
+// Printing an array
+let colors = ["Red", "Green", "Blue"];
+console.log("Colors:", colors);
+
+// Using console.warn for warnings
+console.warn("This is a warning message!");
+
+// Using console.error for errors
+console.error("This is an error message!");
+
+// Using console.table for tabular data
+console.table(user);
